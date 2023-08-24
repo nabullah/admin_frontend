c1da0451bb7ed3ea45fb68f1c47a980efb867097
Nabullah Ansari <nansari@anviam.com>
2023-08-24 19:21:54 +0530

    design changes

--- src/app/admin/admin-sidenav/admin-sidenav.component.ts
+++ src/app/admin/admin-sidenav/admin-sidenav.component.ts
@@ -14,11 +14,20 @@ export class AdminSidenavComponent {
 	auth = inject(AuthService);
 	themeService = inject(ThemeService);
 
+	notificationData: any[] = [
+		{ id: 1, avatar: "", message: "Congratulation Flora!", message1: "Won the monthly best seller badge", time: "10 mins ago" },
+		{ id: 2, avatar: "", message: "New message received", message1: "You have 10 unread messages", time: "11 Aug" },
+		{ id: 3, avatar: "", message: "Received Order", message1: "Won the monthly best seller badge", time: "19 Mar" },
+		{ id: 4, avatar: "", message: "Congratulation Flora!", message1: "Won the monthly best seller badge", time: "19 Mar" },
+		{ id: 5, avatar: "", message: "Congratulation Flora!", message1: "Won the monthly best seller badge", time: "19 Mar" },
+		{ id: 6, avatar: "", message: "Congratulation Flora!", message1: "Won the monthly best seller badge", time: "19 Mar" },
+	];
+
 	logOut() {
 		this.auth.logout();
 	}
 
 	toggleTheme(): void {
 		this.themeService.toggleTheme().then();
-	  }
+	}
 }
